ef231e5320aad8f87d4dd9cc5f3d79493fac4992
Marco Ferreira <marcoavfcc@gmail.com>
2021-05-12 09:43:15 -0300

    atualizado

---
+++ src/contexts/auth.js
@@ -0,0 +1,31 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext({});
+
+export default function AuthProvider({ children }){
+    const [user, setUser] = useState(null);
+    const [loadingAuth, setLoadingAuth] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        function loadStorage() {
+            const storageUser = localStorage.getItem('SistemaUser');
+
+            if(storageUser) {
+                setUser(JSON.parse(storageUser));
+                setLoading(false);
+            }
+
+            setLoading(false);
+        }
+
+        loadStorage()
+
+    },[])
+
+    return(
+        <AuthContext.Provider value={{ signed: !!user, user, loading }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,12 +1,13 @@
+import { useContext } from "react";
 import { Redirect, Route } from "react-router";
+import { AuthContext } from "../contexts/auth";
 
 export default function RouteWrapper({
     component: Component,
     isPrivate,
     ...rest
 }){
-    const loading = false;
-    const signed = false;
+    const { signed, loading } = useContext(AuthContext)
 
     if(loading) {
         return(
